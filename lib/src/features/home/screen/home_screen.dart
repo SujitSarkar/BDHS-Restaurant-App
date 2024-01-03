4ced86026ae6530f42f62bd3b464cc6e64be3122
Sujit Sarkar <snigdho532@gmail.com>
2024-01-03 12:31:19 +0600

    remove unnecessary plugin import

--- lib/src/features/home/screen/home_screen.dart
+++ lib/src/features/home/screen/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/Material.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'package:provider/provider.dart';
